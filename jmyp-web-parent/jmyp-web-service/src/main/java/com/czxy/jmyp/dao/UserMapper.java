9659a673b4e1837049a95273f150c7a7ad613c03
张小仙 <1336910272@qq.com>
2018-12-12 15:46:01 +0800

    pojo

---
+++ jmyp-web-parent/jmyp-web-service/src/main/java/com/czxy/jmyp/dao/UserMapper.java
@@ -0,0 +1,15 @@
+package com.czxy.jmyp.dao;
+
+import com.czxy.jmyp.pojo.User;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @ClassName UserMapper
+ * @Description TODO
+ * @Author 张小仙
+ * @Date 2018/12/12 15:44
+ * @Version 1.0
+ **/
+@org.apache.ibatis.annotations.Mapper
+public interface UserMapper extends Mapper<User> {
+}
