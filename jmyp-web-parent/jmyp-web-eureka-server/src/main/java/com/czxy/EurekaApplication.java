9a39fd17aa74aa3b7f63dca005ef28a441cf96a4
张小仙 <1336910272@qq.com>
2018-12-12 09:24:24 +0800

    健全项目

---
+++ jmyp-web-parent/jmyp-web-eureka-server/src/main/java/com/czxy/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.czxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName EurekaApplication
+ * @Description TODO
+ * @Author 张小仙
+ * @Date 2018/12/12 9:07
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
